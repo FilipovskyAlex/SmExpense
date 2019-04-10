2c15ce0fea78ba1749a46e2df808d77f3e93cfbf
FilipovskyAlex <filipovskiy99@gmail.com>
2019-04-10 18:01:33 +0300

    add jquery, add some settings and compile css to scss

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,4 +1,11 @@
 <head>
     <meta charset="utf-8">
+    <meta name="author" content="Alex Filipovsky">
+    <meta name="description" content="Smart Expense Keeping Web App via Laravel 5.8.*">
     <title>Smart Expense Keeping</title>
+
+    <link rel="shortcut icon" href="/public/img/favicon-16x16.png">
+    <link rel="icon" href="/public/img/favicon-16x16.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="/public/css/style.css">
 </head>
