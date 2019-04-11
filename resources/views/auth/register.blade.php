76a77170848a281db8c252e2af03197ad3121160
FilipovskyAlex <filipovskiy99@gmail.com>
2019-04-11 11:18:01 +0300

    create basic email and reset pages template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -159,6 +159,8 @@
                             </button>
                         </div>
                     </div>
+
+                    <p align="center" style="margin-bottom: 0; padding: 20px 0 0 40px">Already have an account?<a href="{{ route('register') }}" style="padding-left: 10px; color: #f62e75; text-decoration: none;">Sign in</a></p>
                 </form>
             </div>
         </div>
