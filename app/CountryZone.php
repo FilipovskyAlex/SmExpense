ed394f9556c9acebef6fb5a0bd89a13a9c99bd01
FilipovskyAlex <filipovskiy99@gmail.com>
2019-04-12 13:35:44 +0300

    create Country & CountryZone models

---
+++ app/CountryZone.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CountryZone extends Model
+{
+    protected $table = 'country_zones';
+
+    protected $fillable = ['code', 'name'];
+}
