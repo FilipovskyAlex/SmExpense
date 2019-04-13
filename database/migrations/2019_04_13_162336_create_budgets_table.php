7ff23ca1b31e08243bc5cdb8497417da05b9cc00
FilipovskyAlex <filipovskiy99@gmail.com>
2019-04-13 19:46:35 +0300

    create migrations for: company, category, period, role, budget, expense, user-details tables

---
+++ database/migrations/2019_04_13_162336_create_budgets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBudgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('company_id')->unsigned();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->bigInteger('period_id')->unsigned();
+            $table->foreign('period_id')->references('id')->on('periods')->onDelete('cascade');
+            $table->string('item');
+            $table->integer('unit');
+            $table->integer('quantity');
+            $table->integer('budget');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budgets');
+    }
+}
